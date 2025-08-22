53d822f02731d83cbedb875637bb07d00b3b7a14
malakemad16 <maea12900@gmail.com>
2025-08-22 23:39:06 +0300

    Add action to the share icon

--- JS/main.js
+++ JS/main.js
@@ -8,3 +8,16 @@ window.onscroll = () => {
         header.classList.remove("bg-black");
     }
 }
+// Add action to the share icon
+let shareBtn = document.querySelector(".share-btn");
+
+shareBtn.addEventListener('click', () => {
+    const portfolioLink = "http://127.0.0.1:5500/index.html#";
+    navigator.clipboard.writeText(portfolioLink)
+        .then(() => {
+            alert("Portfolio link copied! 🚀");
+        })
+        .catch(err => {
+            console.error("Failed to copy: ", err);
+        });
+});
